b1ead56e7d26e1cedb256146196062cebcea73ae
Nic Foster <nfoster@backflipstudios.com>
2017-12-31 16:04:47 -0700

    Platforms can be created in mid-air now.

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -10,6 +10,7 @@ namespace BoxyJump
 		public float maxHorizontalRange = 20.0f;
 
 		public float m_oddsOfPit = 0.1f;
+		public float m_oddsOfMidAirPlatform = 0.1f;
 
 		private void Start()
 		{
@@ -51,9 +52,22 @@ namespace BoxyJump
 				}
 			}
 
+			foreach (var platformKVP in m_spawnedAirPlatforms)
+			{
+				int platformX = (int)(platformKVP.Value.transform.position.x);
+
+				if (Mathf.Abs(charPosition.x - platformX) > maxHorizontalRange)
+				{
+					Destroy(platformKVP.Value);
+
+					keysToRemove.Add(platformX);
+				}
+			}
+
 			foreach (int key in keysToRemove)
 			{
 				m_spawnedPlatforms.Remove(key);
+				m_spawnedAirPlatforms.Remove(key);
 			}
 
 			SpawnGroundPlatforms(charPosition);
@@ -71,8 +85,8 @@ namespace BoxyJump
 				++nearestEvenX;
 			}
 
-			// No pits near the spawn
-			bool pitsAllowed = !(nearestEvenX < maxHorizontalRange && nearestEvenX > -maxHorizontalRange);
+			// No pits near the spawn, no air platforms near spawn
+			bool changesAllowed = !(nearestEvenX < maxHorizontalRange && nearestEvenX > -maxHorizontalRange);
 
 			for (int i = nearestEvenX - 20; i < nearestEvenX + 20; i += 2)
 			{
@@ -81,7 +95,7 @@ namespace BoxyJump
 					continue;
 				}
 
-				if (pitsAllowed)
+				if (changesAllowed)
 				{
 					int seedForX = System.Math.Abs(m_randSeed ^ i);
 					var randGen = new System.Random(seedForX);
@@ -101,9 +115,36 @@ namespace BoxyJump
 
 				m_spawnedPlatforms.Add(i, platform);
 			}
+
+			if (changesAllowed)
+			{
+				for (int i = nearestEvenX - 20; i < nearestEvenX + 20; i += 2)
+				{
+					if (m_spawnedAirPlatforms.ContainsKey(i))
+					{
+						continue;
+					}
+
+					int seedForX = System.Math.Abs(m_randSeed ^ i);
+					var randGen = new System.Random(seedForX);
+
+					double roll = randGen.NextDouble();
+
+					if (roll < m_oddsOfMidAirPlatform)
+					{
+						float height = (float)randGen.NextDouble() * 5.0f;
+
+						Vector2 spawnPosition = new Vector2(i, height);
+						var platform = Instantiate(m_platform, spawnPosition, Quaternion.identity);
+
+						m_spawnedAirPlatforms.Add(i, platform);
+					}
+				}
+			}
 		}
 
-		private Dictionary<int, GameObject> m_spawnedPlatforms = new Dictionary<int, GameObject>();
+		private Dictionary<int, GameObject> m_spawnedPlatforms = new Dictionary<int, GameObject>(40);
+		private Dictionary<int, GameObject> m_spawnedAirPlatforms = new Dictionary<int, GameObject>(10);
 		private Vector3 m_lastPosition;
 		private int m_randSeed;
 	}
